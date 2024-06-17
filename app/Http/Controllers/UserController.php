03f8f2c1f5759f4ea56dd398f0e1f63c5923895f
Daniel Briceno <daniel.b100726@gmail.com>
2024-06-17 10:34:42 -0400

    added UserController file

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class UserController extends Controller
+{
+    //
+    public function index()
+    {
+        //logica
+        //consultar la BD
+        // Si usamos de laravel 10 en adelante es recomendable trabajar con tipado de datos
+        return '<h1>Hola mundo desde el index de UserController</h1>';
+    }
+
+    public function user(int $user_id): view
+    {
+        //mas o menos equivalente a un console log
+        dd($user_id);
+
+        // var_dump($userId);
+        // die();
+
+        // return '<h1>Hola user desde el user de UserController</h1>';
+        return view('welcome');
+    }
+}
